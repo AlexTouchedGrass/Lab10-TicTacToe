56dd4fee46206314eed2340ff3f7bdf260adc464
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2024-02-15 09:39:29 -0500

    Dear lord, forgive me

--- src/Game.java
+++ src/Game.java
@@ -47,8 +47,8 @@ public class Game {
         //Input Helper
         InputHelper.getPrettyHeader();
         //Variables
-        String playerX = "X"; //Also player 1
-        String playerO = "O"; //Also player 2
+        String player1 = "";
+        String player2 = "";
         boolean allDone = false;
 
 
@@ -57,24 +57,28 @@ public class Game {
             boolean doneStart = false;
             boolean donePlay = false;
 
-            //THIS DO WHILE DECIDES WHO GOES FIRST
+            //THIS DO WHILE DECIDES WHO IS X's and O's
             do {
-                int check = 0;
+                String check = "";
                 System.out.println("\nPlayer 1 is X's and Player 2 is O's.");
                 System.out.println("Please enter 1 or 2 to decide who goes first, 1 for X's and 2 for O's.");
-                check = scan.nextInt();
-                if (check == 1 ) {
-                    System.out.println("Player 1 (X's) will go first.");
+                check = scan.nextLine();
+                if (check.equalsIgnoreCase("X") ) {
+                    System.out.println("Player 1 will be X's.\nPlayer 2 will be O's");
                     doneStart = true;
-                } else if (check == 2){
-                    System.out.println("Player 2 (O's) will go first.");
+                    player1 = "X";
+                    player2 = "O";
+                } else if (check.equalsIgnoreCase("O")){
+                    System.out.println("Player 1 will be O's.\nPlayer 2 will be X's.");
                     doneStart = true;
+                    player1 = "O";
+                    player2 = "X";
                 } else {
-                    System.out.println("That is not a 1 or a 2, try again.");
+                    System.out.println("That is not an X or an O, try again.");
                 }
             } while(!doneStart);
 
-
+            //Start of a game.
             displayBoard();
 
         } while (!allDone);
